4e7639e9f7493a6b8cf2a0caef62b2b355913412
MosstafaSa3ed <msafcicu@gmail.com>
2024-05-11 01:41:55 +0000

    adding helm charts and ingress controllers

---
+++ start.sh
@@ -0,0 +1,31 @@
+
+passwd=$(aws ecr get-login-password --region eu-west-3 | cut -d' ' -f6)
+baseURL=622931036635.dkr.ecr.eu-west-3.amazonaws.com
+airportsRepoURL=$baseURL/viwell-airports
+countriesRepoURL=$baseURL/viwell-countries
+
+aws ecr get-login-password --region eu-west-3 | docker login --username AWS --password-stdin $baseURL
+kubectl delete secret ecr -nviwell
+kubectl create secret -nviwell docker-registry ecr --docker-server=$baseURL --docker-username=AWS --docker-password=$passwd
+
+echo "\n*******build and push docker image for airports******\n"
+docker build -t $airportsRepoURL:1.0.1-v1 ./viwell-airports
+docker push $airportsRepoURL:1.0.1-v1
+
+echo "\n*******build and push helm chart for airports******\n"
+helm package ./viwell-airports/devops-tools/helm
+helm push viwell-airports-1.0.1.tgz oci://$baseURL
+
+echo "\n*******Deploy airports helm chart******\n"
+helm upgrade --install viwell-airports -nviwell oci://$airportsRepoURL --version 1.0.1
+
+echo "\n*******build and push docker image for countries******\n"
+docker build -t $countriesRepoURL:1.0.1-v1 ./viwell-countries
+docker push $countriesRepoURL:1.0.1-v1
+
+echo "\n*******build and push helm chart for countries******\n"
+helm package ./viwell-countries/devops-tools/helm
+helm push viwell-countries-1.0.1.tgz oci://$baseURL
+
+echo "\n*******Deploy countries helm chart******\n"
+helm upgrade --install viwell-countries -nviwell oci://$countriesRepoURL --version 1.0.1
